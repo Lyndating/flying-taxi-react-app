55048132c1c66ad743cf016548041f938b3ff011
Ting Luo <shirleyayu@hotmail.com>
2022-05-19 23:10:47 +1000

    second section of home page

--- flying-taxi-react/src/App.js
+++ flying-taxi-react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SignUpForm from './components/SignUpForm';
+// import SignUpForm from './components/SignUpForm';
 
 import './App.css';
 import {HashRouter as Router, Route, Routes, Link} from 'react-router-dom';
@@ -24,7 +24,7 @@ function App() {
     <div className="App">
       {routes}
   
-      <SignUpForm />
+      {/* <SignUpForm /> */}
     </div>
   );
 }
--- flying-taxi-react/src/components/Home.js
+++ flying-taxi-react/src/components/Home.js
@@ -2,14 +2,32 @@ import React, {useState, useTransition} from 'react';
 import "./Home.css";
 import{Link} from 'react-router-dom';
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+    //The maximum is inclusive and the minimum is inclusive 
+};
 const Home = () => {
     const [startLocation, setStartLocation] = useState("");
     const [destination, setDestination] = useState('');
     const [result, setResult] = useState(false);
-    const _sumbithandler = ()=>{
-        setResult = true;
-    };
+    const [price, setPrice]= useState(0);
+    const _sumbithandler = (e)=>{
+        e.preventDefault();
+        setResult(true);
+        setPrice(getRandomIntInclusive(20,60));
 
+    };
+    const _closePopUp = ()=>{
+        setResult(false);
+    }
+    let cssStyleClassName = "";
+    if (result){
+        cssStyleClassName="pop-up-price-show";
+    }else{
+        cssStyleClassName="pop-up-price-hide";
+    }
 
     return (
         <>
@@ -26,25 +44,37 @@ const Home = () => {
             </div>
         </div>
         <div className='Service-page'>
-            <h3>Check the pricing</h3>
+            <h2>Check the pricing</h2>
             <form onSubmit={_sumbithandler}>
+                <div className='check-start'>
                 <i className='fa-solid fa-location-dot'></i>
-                <label>Start: 
+                <label> Start: 
+                <br></br>
+                <br></br>
                 <input type="text" value={startLocation} onChange={(e)=> setStartLocation(e.target.value)}required/>         
                 </label>
-                <span>You are here</span>
-                <label>End: 
+                <br></br>
+                <br></br>
+                </div>
+                <div className='check-destination'>
+                <i className='fa-solid fa-location-dot'></i>
+                <label> Destination:
+                    <br></br>
+                    <br></br>
                 <input type="text" value={destination} onChange={(e)=> setDestination(e.target.value)} required/>
                 </label>
+                <br></br>
+                <br></br>
+                </div>
                 <button>Check</button>
             </form>
         </div>
         {result && 
-            <div className='pop-up-price'>
+            <div className={cssStyleClassName}>
                 <p>From : {startLocation}</p>
                 <p>To: {destination}</p>
-                <p>Price: $ {26.58}</p>
-                <button>Close</button>
+                <p>Price: $ {price}</p>
+                <button onClick={_closePopUp}>Close</button>
             </div>}
         </>
     )
